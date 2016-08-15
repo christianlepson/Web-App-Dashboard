4ec0db5a5b9c1a4786731b26c200da5326b60b71
Christian Lepson <christian.lepson@gmail.com>
2016-08-15 13:47:15 -0400

    Made new members table, added 'sent' when message is sent

--- js/application.js
+++ js/application.js
@@ -1,13 +1,15 @@
 $(function() {
+	var $messageDiv = $('#message');
 	var $alerts = $('#alerts');
 	var $headerNotify = $('#header-alert-notify');
+	var $messageSubmitBtm = $('#message-submit');
 
 	//alertUser creates notifications with click events
 	var alertUser = function() {
 		//Get notification text
 		var notifyText = "Nullam sagittis venenatis enim, vitae efficitur ligula volutpat ut.";
 		//Append Notification (with close button) to Alerts div
-		$alerts.append('<div class="notification"><p><strong>Alert</strong> ' + 
+		$alerts.append('<div class="notification"><p><strong>Alert</strong></p>' + 
 			notifyText + '</p><span class="close"></span></div>');
 		//Close Notification when close button is clicked
 		$(".close").on('click', function() {
@@ -21,7 +23,11 @@ $(function() {
 		});
 	};
 
+	// Event handler for user message submission
+	var messageHandler = function() {
+			$messageDiv.append('<div class="msgSuccess"><p>Sent</p>');
+	};
 
-	
+	$messageSubmitBtm.on('click', messageHandler);
 	alertUser();
 });
